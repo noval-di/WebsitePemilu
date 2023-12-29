fcd56544a1a484ae143e8d93a31567d17716584a
noval-di <novaldiituaja@gmail.com>
2023-12-29 23:57:16 +0700

    add admin panels

--- WebsitePemilu/Pemilu/admin.py
+++ WebsitePemilu/Pemilu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from Pemilu.models import DataKoordinator, DataPemilih, DataWarga
 # Register your models here.
+admin.site.register(DataKoordinator)
+admin.site.register(DataPemilih)
+admin.site.register(DataWarga)
