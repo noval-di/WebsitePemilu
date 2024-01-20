2bf8f48c5ed1d05f65e30ee1a11b183596244004
Ubuntu <ubuntu@ip-172-26-14-74.ap-southeast-1.compute.internal>
2024-01-20 19:34:47 +0000

    add admin DataPemilu

--- WebsitePemilu/Pemilu/admin.py
+++ WebsitePemilu/Pemilu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from Pemilu.models import DataKoordinator, DataPemilih, DataWarga
+from Pemilu.models import DataKoordinator, DataPemilih, DataWarga, DataPemilu
 # Register your models here.
 admin.site.register(DataKoordinator)
 admin.site.register(DataPemilih)
 admin.site.register(DataWarga)
+admin.site.register(DataPemilu)
